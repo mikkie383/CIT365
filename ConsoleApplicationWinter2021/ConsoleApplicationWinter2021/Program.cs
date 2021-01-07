832daa84d61f9ab069ff72ee5800e220f83e69a9
mikkie383 <tsa16002@byui.edu>
2021-01-07 16:01:48 -0700

    Finished First Visual Studio C# Application

--- ConsoleApplicationWinter2021/ConsoleApplicationWinter2021/Program.cs
+++ ConsoleApplicationWinter2021/ConsoleApplicationWinter2021/Program.cs
@@ -21,17 +21,29 @@ namespace ConsoleApplicationWinter2021
             //4.
             DateTime currentDate = DateTime.Now;
             //Optional var currentDate = DateTime.Now;
-            Console.WriteLine("The current date is " + currentDate.ToString("d"));
+            Console.WriteLine($"The current date is {currentDate.ToString("d")}");
 
-            //5.
+            //5.Output the number of days until Christmas this year(2021)
+            DateTime christmas = new DateTime(2021, 12, 25);
+            Console.WriteLine($"The number of days until Christmas: {Math.Round((christmas - currentDate).TotalDays)}");
 
+            //6.Add the program example from section 2.1
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+            Console.WriteLine("Please enter the width:");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+            Console.WriteLine("Please enter the height:");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+            Console.WriteLine("The length of the wood is " +
+            woodLength + " feet");
+            Console.WriteLine("The area of the glass is " +
+            glassArea + " square metres");
 
-            //6.
-
-            
             //7.
-
-
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
         }
